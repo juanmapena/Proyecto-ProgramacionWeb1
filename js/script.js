cccbfaef9d2b7568b9a065505b46d515a7560730
juanmapena <juanmanuelpena180@gmail.com>
2025-11-11 17:44:14 -0300

    Se modifico el modal

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 import { FormularioContacto } from "../js/contacto.js";
 import { SimuladorPago } from "../js/formasdepago.js";
 import { PersonalizadorTarjetaRegalo } from "../js/giftcarsd.js";
+import {Detalle} from './detalle.js'
+import {CursosSlider} from './slider.js'
+import {Busqueda} from './busqueda.js'
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -8,8 +11,15 @@ document.addEventListener('DOMContentLoaded', function () {
     const appPago = new SimuladorPago();
     const appGiftcard = new PersonalizadorTarjetaRegalo();
 
+    const detalle = new Detalle();
+    const slider = new CursosSlider();
+    const busqueda = new Busqueda();
 
 
+
+    busqueda.render();
+    detalle.render();
+    slider.render();
     appContacto.render();
     appPago.render();
     appGiftcard.render();
