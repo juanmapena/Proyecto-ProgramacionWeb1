e798f4fe04e9f11f6cabae4a077ebf304afa91b6
Emanuel Ruiz Díaz <emanuelruizdiazok@gmail.com>
2025-11-11 14:32:53 -0300

    Se agregó el script.js, contacto.js y se modificaron los html y css correspondientes a contacto

---
+++ js/script.js
@@ -0,0 +1,8 @@
+import { FormularioContacto } from "../js/contacto.js";
+
+document.addEventListener('DOMContentLoaded', function() {
+    const appContacto = new FormularioContacto();
+
+    
+    appContacto.render();
+});
