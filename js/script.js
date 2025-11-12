6717ae661e09f0e78743dcf13299fcacbd322edb
juanmapena <juanmanuelpena180@gmail.com>
2025-11-11 21:01:15 -0300

    Se agrega modificacion de js en boton del header y el carrito dinamico, con sus estilos

--- js/script.js
+++ js/script.js
@@ -4,24 +4,24 @@ import { PersonalizadorTarjetaRegalo } from "../js/giftcarsd.js";
 import {Detalle} from './detalle.js'
 import {CursosSlider} from './slider.js'
 import {Busqueda} from './busqueda.js'
+import { BotonSesionHeader } from "./boton-sesion-header.js";
 
 
 document.addEventListener('DOMContentLoaded', function () {
+
     const appContacto = new FormularioContacto();
     const appPago = new SimuladorPago();
     const appGiftcard = new PersonalizadorTarjetaRegalo();
-
     const detalle = new Detalle();
     const slider = new CursosSlider();
     const busqueda = new Busqueda();
-
-
+    const botonSesionHeader = new BotonSesionHeader();
 
     busqueda.render();
     detalle.render();
     slider.render();
     appContacto.render();
     appPago.render();
-    appGiftcard.render();
+    botonSesionHeader.render();
 });
 
